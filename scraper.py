7ef91032e2f12f63043b0ee976527be8e936836c
albinchristo04 <priyanka0101111@gmail.com>
2025-10-22 21:12:43 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -38,12 +38,32 @@ class StreamScraper:
         
         try:
             chrome_options = Options()
-            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--headless=new')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--window-size=1920,1080')
+            chrome_options.add_argument('--disable-blink-features=AutomationControlled')
             chrome_options.add_argument(f'user-agent={self.headers["User-Agent"]}')
+            
+            # Try to find Chrome binary in common locations
+            import shutil
+            chrome_paths = [
+                '/usr/bin/google-chrome',
+                '/usr/bin/google-chrome-stable',
+                '/usr/bin/chromium-browser',
+                '/usr/bin/chromium',
+            ]
+            
+            chrome_binary = None
+            for path in chrome_paths:
+                if shutil.which(path.split('/')[-1]):
+                    chrome_binary = shutil.which(path.split('/')[-1])
+                    break
+            
+            if chrome_binary:
+                chrome_options.binary_location = chrome_binary
+                print(f"Using Chrome binary: {chrome_binary}")
             
             driver = webdriver.Chrome(options=chrome_options)
             return driver
